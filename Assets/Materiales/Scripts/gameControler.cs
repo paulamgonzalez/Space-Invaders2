88b71b7bdc175699799c6c46fc89c92fd6fa845c
paulamgonzalez <90839118+paulamgonzalez@users.noreply.github.com>
2021-10-20 18:18:46 +0100

    Script
    Script: añadido otra vez que al dar espacio cambie a la segunda pantalla y pequeños cambios

--- Assets/Materiales/Scripts/gameControler.cs
+++ Assets/Materiales/Scripts/gameControler.cs
@@ -10,6 +10,8 @@ public class gameControler : MonoBehaviour
 
     public GameObject introScreen;
     public GameObject pressAnyButtonScreen;
+    public GameObject pantallaInicial;
+
     // Start is called before the first frame update
 
     void Awake()
@@ -32,15 +34,32 @@ public class gameControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (introScreen.activeSelf)
         {
-            gameControler.instance.EnterIntro();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                gameControler.instance.EnableScreenAnybutton();
+            }
+            
         }
+
+       
+
     }
 
-    public void EnterIntro()
+
+    //Entrar a la pantalla de espera
+    public void EnableScreenAnybutton()
     {
         introScreen.SetActive(false);
         pressAnyButtonScreen.SetActive(true);
+        
+    }
+
+    public void EnablePantallaInicial()
+    {
+        introScreen.SetActive(false);
+        pressAnyButtonScreen.SetActive(false);
+        pantallaInicial.SetActive(true);
     }
 }
